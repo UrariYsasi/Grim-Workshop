c06bcdc07724c764d554de0dcd269483fa8f5372
Declan Hopkins <hopkins.declan@gmail.com>
2016-02-09 20:44:06 -0500

    Fix Stockpile origin and hit box

--- Peon/Source/Game/Entity/Stockpile.cpp
+++ Peon/Source/Game/Entity/Stockpile.cpp
@@ -6,6 +6,8 @@
 Stockpile::Stockpile(Game* game, Vector2D position) :
     Prop(game, position)
 {
+    m_origin = Vector2D(16, 16);
+    m_hitBox = Rectangle(-16, -16, 32, 32);
 }
 
 Stockpile::~Stockpile()
@@ -24,5 +26,5 @@ void Stockpile::Update(double deltaTime)
 void Stockpile::Render()
 {
     Renderer* renderer = m_game->GetRenderer();
-    renderer->RenderSprite("bonfire_4", 0, 0, (int)m_position.x, (int)m_position.y, (int)SPRITE_SIZE.x, (int)SPRITE_SIZE.y);
+    renderer->RenderSprite("bonfire_4", 0, 0, (int)(m_position.x - m_origin.x), (int)(m_position.y - m_origin.y), (int)SPRITE_SIZE.x, (int)SPRITE_SIZE.y);
 }
