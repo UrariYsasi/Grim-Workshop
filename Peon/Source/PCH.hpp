08d20243416e72a041a4fdb47bf00177bf6dd3f7
Declan Hopkins <hopkins.declan@gmail.com>
2016-03-13 21:10:46 -0400

    Failed attempt to fix Linux port

--- Peon/Source/PCH.hpp
+++ Peon/Source/PCH.hpp
@@ -37,9 +37,9 @@
     #include <SDL_mixer.h>
 #else
     #include <SDL2/SDL.h>
-    #include <SDL2_image/SDL_image.h>
-    #include <SDL2_ttf/SDL_ttf.h>
-    #include <SDL2_mixer/SDL_mixer.h>
+    #include <SDL2/SDL_image.h>
+    #include <SDL2/SDL_ttf.h>
+    #include <SDL2/SDL_mixer.h>
     #include <SDL2/SDL_opengl.h>
 #endif
 
