834d77a4a96701fdbc36514824ad57af0546b736
Declan Hopkins <hopkins.declan@gmail.com>
2016-10-13 12:21:52 -0400

    Fix Asset ID creation

--- peon_engine/Source/Assets/Importer/Importer.cpp
+++ peon_engine/Source/Assets/Importer/Importer.cpp
@@ -23,7 +23,27 @@ Importer::~Importer()
 
 std::string Importer::CreateId(std::string const& filePath) const
 {
-    return filePath;
+    std::string id = filePath;
+    std::string assetDirectoryPath = ASSET_DIRECTORY_PATH;
+
+    // Remove the directory path
+    std::string::size_type pos = id.find(assetDirectoryPath);
+    if (pos != std::string::npos)
+    {
+        id.erase(pos, assetDirectoryPath.length());
+    }
+
+    // Change slashes to dots
+    std::replace(id.begin(), id.end(), '/', '.');
+
+    // Remove the file extension
+    std::string::size_type extensionPos = id.find_last_of(".");
+    if (extensionPos != std::string::npos)
+    {
+        id = id.substr(0, extensionPos);
+    }
+
+    return id;
 }
 
 }
